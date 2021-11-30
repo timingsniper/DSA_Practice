9fe2cc07042f6b564cd378df551987b591ab0133
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-12-01 01:28:16 +0800

    Update 0205:多项式加法.cpp

--- 第二章/0205:多项式加法.cpp
+++ 第二章/0205:多项式加法.cpp
@@ -13,38 +13,41 @@
 #include <map>
 #include <deque>
 #include <queue>
+#include <stack>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
-int main() {
-	int n;
-	
-	cin >> n;
-	
 
-	while (n--) {
-		int i = 2, coeff, power;
-		map<int, int, greater<int>> poly; //Polynomial declared with map STL
-		while (i--) {
-			while (1) {
+int n, coeff, power;
+
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		map<int, int, greater<int>>poly;
+		for (int j = 0; j < 2; j++) {
+			while (true) {
 				cin >> coeff >> power;
 				if (power < 0) {
 					break;
-				}
-				if (poly.find(power) != poly.end()) {
+				} 
+				if (poly.find(power) != poly.end()) { //poly에서 값을 못찾으면 .end()를 반환하는점 이용, 즉 찾았다면
 					poly[power] += coeff;
 				}
 				else {
 					poly.insert(make_pair(power, coeff));
 				}
-
 			}
 		}
-		for (auto& item : poly) {
-			if (item.second != 0) {
-				cout << "[ " << item.second << " " << item.first << " ] ";
-			}
+
+		map<int, int, greater<int>>::iterator iter;
+		for (iter = poly.begin(); iter != poly.end(); iter++) {
+			if (iter->second == 0) { continue; }
+			cout << "[ " << iter->second << " " << iter->first << " ] ";
 		}
 		cout << endl;
+
 	}
 }
