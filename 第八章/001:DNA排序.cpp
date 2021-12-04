ecf4d889ac0473eb259536618faeab7f7f2c1b9f
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-12-05 00:34:04 +0800

    Update 001:DNA排序.cpp

--- 第八章/001:DNA排序.cpp
+++ 第八章/001:DNA排序.cpp
@@ -17,16 +17,23 @@
 #include <fstream>
 #include <sstream>
 
+
+/*
+* Big Idea:
+* DNA를 struct로 만든다. 그후 비교연산자 < 만들어주고, 불안하면 >까지
+* reversepair는 문제대로 만들어주면 돼고, sort(dnas, dnas+m)이 포인트.
+* 
+*
+* 
+*/
 using namespace std;
 
-//map<int, string, less<int>>dna;
-int n, m;
-
-struct dnaStruct {
-	string seq;
+int strLen, m;
+struct DNA {
+	string pattern;
 	int pairNum;
 	int inputOrder;
-	friend bool operator<(const dnaStruct a, const dnaStruct b) {
+	friend bool operator<(const DNA a, const DNA b) {
 		if (a.pairNum != b.pairNum) {
 			return a.pairNum < b.pairNum;
 		}
@@ -35,37 +42,43 @@ struct dnaStruct {
 		}
 	}
 
+	friend bool operator>(const DNA a, const DNA b) {
+		if (a.pairNum != b.pairNum) {
+			return a.pairNum > b.pairNum;
+		}
+		else{
+			return a.inputOrder > b.inputOrder;
+		}
+	}
+
+	
 }dnas[100];
 
-int getReversePair(string pattern) {
+
+int getReversePair(string ptr) {
 	int ans = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = i + 1; j < n; j++) {
-			if (pattern[i] > pattern[j]) {
+	for (int i = 0; i < strLen; i++) {
+		for (int j = i + 1; j < strLen; j++) {
+			if (ptr[i] > ptr[j]) {
 				ans++;
 			}
 		}
 	}
-
 	return ans;
-
 }
 
-
 int main() {
-	cin >> n >> m;
+	cin >> strLen >> m;
 	for (int i = 0; i < m; i++) {
-		string sequence;
-		cin >> dnas[i].seq;
-		int revVal = getReversePair(dnas[i].seq);
+		string patt;
+		cin >> dnas[i].pattern;
+		int revNum = getReversePair(dnas[i].pattern);
+		dnas[i].pairNum = revNum;
 		dnas[i].inputOrder = i;
-		dnas[i].pairNum = revVal;
 	}
 
 	sort(dnas, dnas + m);
-
 	for (int i = 0; i < m; i++) {
-		cout << dnas[i].seq << endl;
+		cout << dnas[i].pattern << endl;
 	}
-
 }
