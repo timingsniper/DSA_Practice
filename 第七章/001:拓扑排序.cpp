125f0aec1830afabb620dd16688ca7730d6d3a0b
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-12-04 15:57:56 +0800

    Update 001:拓扑排序.cpp

--- 第七章/001:拓扑排序.cpp
+++ 第七章/001:拓扑排序.cpp
@@ -17,46 +17,61 @@
 #include <fstream>
 #include <sstream>
 
-using namespace std;
 
+/*
+* Big Idea:
+* Topological Sorting은 먼저 indegree가 0인놈을 없애고, 없앨때 걔로부터 연결되있는 선들을 지워주고, 그친구들의 indegree도 1 줄여주는것이 필요.
+* 입력을 받을때, graph[from-1][to-1]위치에 1 값을 넣어 변을 표시한다. 동시에 to 값의 vertex의 indegree를 1 늘려준다. 세팅완료.
+* 
+* 모든 꼭짓점이 프린트되는게 목표니, printcount를 0부터 시작해 프린트되면 1씩 늘리고, printCount값이 v값과 동일해지면 루프를 종료한다.
+* 내부에서 일단 i기준 v값까지 루프, 꼭짓점 i의 indegree가 0이면 프린트및 gotPrinted 변경, printCount도 1 늘린다.
+* 만약 조건이 만족되어 i가 프린트 되었다면, j기준 v까지 루프하여 연결되었던 변들 타노스시키기 시작. indegree[j] && i != j && !gotPrinted[j]면 조건충족이다. 
+* 이때도 printcount와 gotprinted 값 업데이트를 하도록 한다.
+*
+* 
+*/
+using namespace std;
 int inDegree[100];
-int sorted[100];
-int graph[100][100]; //v*v array
-bool gotPrinted[100];
+int graph[100][100];
+int gotPrinted[100];
 
 
 int main() {
-	int v, a, first, second, printCount = 0;
+	memset(inDegree, 0, sizeof(inDegree));
+	memset(graph, 0, sizeof(graph));
+	memset(gotPrinted, 0, sizeof(gotPrinted));
+	int v, a, printCount = 0;
 	cin >> v >> a;
-	memset(inDegree, 0, sizeof inDegree);
-	memset(graph, 0, sizeof graph);
-	memset(gotPrinted, 0, sizeof gotPrinted);
 	for (int i = 0; i < a; i++) {
-		cin >> first >> second;
-		inDegree[second - 1]++;
-		graph[first-1][second-1] = 1;
+		int from, to;
+		cin >> from >> to;
+		inDegree[to - 1]++;
+		graph[from - 1][to - 1] = 1;
+
 	}
-	int i = 0;
+	//int i = 0;
 	while (printCount != v) {
 		for (int i = 0; i < v; i++) {
-		
-			if (inDegree[i] == 0 && !gotPrinted[i]) { //If indegree of the vertice = 0
+			if (inDegree[i] == 0 && !gotPrinted[i]) {
 				cout << "v" << i + 1 << " ";
 				printCount++;
 				gotPrinted[i] = 1;
 			}
-			for (int j = 0; j < v; j++) {
-				if (graph[i][j] == 1) {
-					inDegree[j]--;
-					if (inDegree[j] == 0 && j != i && !gotPrinted[j]) {
-						cout << "v" << j + 1 << " ";
-						printCount++;
-						gotPrinted[j] = 1;
+			if (gotPrinted[i]) {
+				for (int j = 0; j < v; j++) {
+					if (graph[i][j] == 1) {
+						inDegree[j]--;
+						if (inDegree[j] == 0 && j != i && !gotPrinted[j]) {
+							cout << "v" << j + 1 << " ";
+							printCount++;
+							gotPrinted[j] = 1;
+						}
 					}
-				}
-				graph[i][j] = -1;
 
+				}
 			}
 		}
+
 	}
+
 }
