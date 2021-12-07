6a31d43357907d919d20cc88f1da7cd8b480ad06
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-12-07 11:08:12 +0800

    Update 002:距离排序.cpp
    sort 사용 가능 하도록 후처리

--- 第八章/002:距离排序.cpp
+++ 第八章/002:距离排序.cpp
@@ -12,72 +12,79 @@
 #include <stdio.h>
 #include <map>
 #include <deque>
-#include <queue>
 #include <stack>
 #include <fstream>
 #include <sstream>
+#include <assert.h>
 
 using namespace std;
-
 int n;
 
-struct point {
-    int xP, yP, zP;
+struct point{
+	int xP, yP, zP;
+	int inputOrder;
+	//point(int x, int y, int z): xP(x), yP(y), zP(z){};
 }points[10];
 
-struct distance {
-    point firstPoint, secondPoint;
-    double val;
+struct distance{
+	double distVal; point firstP; point secondP;
+	friend bool operator<(const distance a, const distance b){
+		if(a.distVal != b.distVal){
+			return a.distVal > b.distVal;
+		}
+		else if(a.distVal == b.distVal){
+			if(a.firstP.inputOrder!= b.firstP.inputOrder){
+				return a.firstP.inputOrder < b.firstP.inputOrder;
+			}
+			else{
+				return a.secondP.inputOrder < b.secondP.inputOrder;
+			}
+		}
+		
+			//return a.firstP.inputOrder < b.firstP.inputOrder && a.secondP.inputOrder < b.secondP.inputOrder;
+	}
+}distances[50];
 
-}distances[100];
-
-double getDistance(point a, point b) {
-    double xDiff = pow(a.xP - b.xP, 2.0);
-    double yDiff = pow(a.yP - b.yP, 2.0);
-    double zDiff = pow(a.zP - b.zP, 2.0);
-    return sqrt(xDiff + yDiff + zDiff);
+double getDistance(point a, point b){
+	int xV = pow(a.xP-b.xP, 2.0);
+	int yV = pow(a.yP-b.yP, 2.0);
+	int zV = pow(a.zP-b.zP, 2.0);
+	return sqrt(xV+yV+zV);
 }
 
-int main() {
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> points[i].xP >> points[i].yP >> points[i].zP;
-    }
-
-    int distanceIndex = 0;
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            distances[distanceIndex].firstPoint = points[i];
-            distances[distanceIndex].secondPoint = points[j];
-            distances[distanceIndex].val = getDistance(points[i], points[j]);
-            distanceIndex++;
-        }
-    }
-    struct distance temp;
-    for (int i = 0; i < distanceIndex; i++) {
-        int flag = 1;
-        for (int j = 0; j < distanceIndex - i; j++) {
-            if (distances[j].val < distances[j + 1].val) {
-                flag = 0;
-                temp = distances[j];
-                distances[j] = distances[j + 1];
-                distances[j + 1] = temp;
-
-            }
-        }
-        if (flag) {
-            break;
-        }
-    }
-
-    for (int i = 0; i < distanceIndex; i++) {
-        cout << "(" << distances[i].firstPoint.xP << "," <<
-            distances[i].firstPoint.yP << "," << distances[i].firstPoint.zP
-            << ")-(" << distances[i].secondPoint.xP << "," <<
-            distances[i].secondPoint.yP << "," << distances[i].secondPoint.zP
-            << ")=" << fixed << setprecision(2) << distances[i].val
+int main(){
+	cin >> n;
+	for(int i = 0; i<n; i++){
+		cin >> points[i].xP >> points[i].yP >> points[i].zP; 
+		points[i].inputOrder = i;
+	}
+	int distCount = 0;
+	for(int i = 0; i<n-1; i++){
+		for(int j = i+1; j<n; j++){
+			distances[distCount].firstP = points[i];
+			distances[distCount].secondP = points[j];
+			distances[distCount].distVal = getDistance(points[i], points[j]);
+			distCount++;
+		}
+	}	
+	
+	sort(distances, distances+distCount);
+	for (int i = 0; i < distCount; i++) {
+        cout << "(" << distances[i].firstP.xP << "," <<
+            distances[i].firstP.yP << "," << distances[i].firstP.zP
+            << ")-(" << distances[i].secondP.xP << "," <<
+            distances[i].secondP.yP << "," << distances[i].secondP.zP
+            << ")=" << fixed << setprecision(2) << distances[i].distVal
             << endl;
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
